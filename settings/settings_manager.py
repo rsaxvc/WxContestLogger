916c239ac7ec4de6ea4f0672d06526786b87cc6c
Richard Allen <rsaxvc@gmail.com>
2013-05-18 23:34:07 -0500

    move settings to a subdirectory

--- settings/settings_manager.py
+++ settings/settings_manager.py
@@ -7,18 +7,21 @@ class settings_manager:
 	"a non-volatile key-value store"
 	def __init__(_self):
 		"open settings file and create a default if needed"
-		_self.filename = os.path.expanduser("~")+"/.wxlogger"
+		dirpath = os.path.expanduser("~")+"/.wxlogger/"
+		_self.configpath = dirpath + "config"
+		if not os.path.exists(dirpath):
+			os.makedirs(dirpath)
 		_self.settings = settings_io.settings_io()
 		try:
-			_self.settings.load( _self.filename )
+			_self.settings.load( _self.configpath )
 		except IOError:
 			pass
 		if( _self.settings.get( "uuid" ) == "" ):
 			_self.settings.put( "uuid", uuid4() )
-			_self.settings.save( _self.filename )
+			_self.settings.save( _self.configpath )
 
 	def save( _self ):
-		_self.settings.save( _self.filename )
+		_self.settings.save( _self.configpath )
 
 	def get( _self, key ):
 		return _self.settings.get( key )
