a14401c487bc724b7a3797370b48d65580b0d9a5
Richard Allen <rsaxvc@gmail.com>
2013-05-20 00:00:13 -0500

    expand db definition

--- db/db_manager.py
+++ db/db_manager.py
@@ -5,5 +5,6 @@ class db_manager:
 	conn = sqlite3.connect(paths.dbdir+'contacts.db')
 
 	"PRAGMA foreign_keys = ON;"
-	"CREATE TABLE known_clients( uuid CHAR(36) PRIMARY KEY ASC, revision INTEGER);"
-	"CREATE TABLE contacts( client_uuid INTEGER NOT NULL, client_rec INTEGER, mycall TEXT, theircall TEXT, foreign key( client_uuid ) references known_clients( rowid ), PRIMARY KEY( client_uuid, client_rec ) );"
+	"CREATE TABLE clients( uuid CHAR(36) PRIMARY KEY ASC, seq INTEGER, name TEXT );"
+	"CREATE TABLE differences( client_uuid INTEGER NOT NULL, client_seq INTEGER NOT NULL, json TEXT, FOREIGN KEY( client_uuid ) REFERENCES clients( rowid ), PRIMARY KEY( client_uuid, client_seq )  );"
+	"CREATE TABLE contacts( client_uuid INTEGER NOT NULL, client_rec INTEGER, mycall TEXT, theircall TEXT, foreign key( client_uuid ) references clients( rowid ), PRIMARY KEY( client_uuid, client_rec ) );"
