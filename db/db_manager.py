906bde9d4a12c92a8ea9accb36d5a6bc7a3a2cfe
Richard Allen <rsaxvc@gmail.com>
2013-06-01 19:35:51 -0500

    add INE to table creation

--- db/db_manager.py
+++ db/db_manager.py
@@ -5,6 +5,6 @@ class db_manager:
 	conn = sqlite3.connect(paths.dbdir+'contacts.db')
 
 	"PRAGMA foreign_keys = ON;"
-	"CREATE TABLE clients( uuid CHAR(36) PRIMARY KEY ASC, seq INTEGER, name TEXT );"
-	"CREATE TABLE differences( client_uuid INTEGER NOT NULL, client_seq INTEGER NOT NULL, json TEXT, FOREIGN KEY( client_uuid ) REFERENCES clients( rowid ), PRIMARY KEY( client_uuid, client_seq )  );"
-	"CREATE TABLE contacts( client_uuid INTEGER NOT NULL, client_rec INTEGER, mycall TEXT, theircall TEXT, foreign key( client_uuid ) references clients( rowid ), PRIMARY KEY( client_uuid, client_rec ) );"
+	"CREATE TABLE IF NOT EXISTS clients( uuid CHAR(36) PRIMARY KEY ASC, seq INTEGER, name TEXT );"
+	"CREATE TABLE IF NOT EXISTS differences( client_uuid INTEGER NOT NULL, client_seq INTEGER NOT NULL, json TEXT, FOREIGN KEY( client_uuid ) REFERENCES clients( rowid ), PRIMARY KEY( client_uuid, client_seq )  );"
+	"CREATE TABLE IF NOT EXISTS contacts( client_uuid INTEGER NOT NULL, client_rec INTEGER, mycall TEXT, theircall TEXT, foreign key( client_uuid ) references clients( rowid ), PRIMARY KEY( client_uuid, client_rec ) );"
