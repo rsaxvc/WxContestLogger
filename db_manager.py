8fe7862593ed9c06aa375ab558e504f6fd330c98
Richard Allen <rsaxvc@gmail.com>
2013-06-08 10:02:07 -0500

    Fix foreign key constraint problem on insert

--- db_manager.py
+++ db_manager.py
@@ -7,7 +7,10 @@ class db_manager:
 
 		c = self.conn.cursor()
 		c.execute( "PRAGMA foreign_keys = ON" )
-		c.execute( "CREATE TABLE IF NOT EXISTS clients( uuid CHAR(36) PRIMARY KEY ASC, seq INTEGER, name TEXT )" )
+		#This rowid declaration is special.
+		#By declaring it as Integer Primary Key, we can now use it
+		#as the target of foreign key constraints
+		c.execute( "CREATE TABLE IF NOT EXISTS clients( rowid INTEGER PRIMARY KEY, uuid CHAR(36) UNIQUE, seq INTEGER, name TEXT )" )
 		c.execute( "CREATE TABLE IF NOT EXISTS differences( client_uuid INTEGER NOT NULL, client_seq INTEGER NOT NULL, json TEXT, FOREIGN KEY( client_uuid ) REFERENCES clients( rowid ), PRIMARY KEY( client_uuid, client_seq )  )" )
 		c.execute( "CREATE TABLE IF NOT EXISTS contacts( client_uuid INTEGER NOT NULL, client_rec INTEGER, mycall TEXT, theircall TEXT, foreign key( client_uuid ) references clients( rowid ), PRIMARY KEY( client_uuid, client_rec ) )" )
 
